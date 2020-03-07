9ac37eab702ade57d0ea11207c5b6c783ffa81d3
Koos van Strien <vstrien@192.168.2.201>
2020-03-07 22:42:00 +0100

    Slowly working through this. Handled some weird bugs already. On a roll :-).

--- daxrevisited/table.py
+++ daxrevisited/table.py
@@ -2,14 +2,21 @@ class Table:
     """One or more columns with datatypes"""
 
     class Column:
+        """A column with a datatype and possibly data"""
+        
         POSSIBLE_DATA_TYPES = ["binary", "boolean", "currency", "datetime", "decimal", "integer", "string", "variant"]
 
-        """A column with a datatype and possibly data"""
         def __init__(self, datatype):
             if datatype not in self.POSSIBLE_DATA_TYPES:
                 raise self.InvalidDatatypeError("{0} is not a valid datatype. Valid datatypes are: {1}".format(datatype, ", ".join(self.POSSIBLE_DATA_TYPES)))
             self.datatype = datatype
             self.values = []
+
+        @classmethod
+        def from_values(cls, datatype: str = "string", values = []):
+            newColumn = cls(datatype)
+            newColumn.values = values
+            return newColumn
 
         class InvalidDatatypeError(Exception):
             """Exception raised when data type is not valid for this column"""
@@ -17,7 +24,7 @@ class Table:
                 self.message=message
 
     def __init__(self, columns: list = []):
-        self.columns = columns
+        self.columns = columns.copy()
         self.n_rows = len(self.columns[0].values) if len(self.columns) > 0 else 0
 
     def add_column(self, datatype: str = "string", default_value = ""):
@@ -26,3 +33,9 @@ class Table:
 
         for i in range(self.n_rows):
             self.columns[-1].values[i] = default_value
+
+    def add_column_with_values(self, datatype: str = "string", values: list = []):
+        """Add a column to the table"""
+        #print("{0}, {1}".format(datatype, ",".join(values)))
+        newColumn = self.Column.from_values(datatype, values)
+        self.columns.append(newColumn)
--- test/test_daxdatamodel.py
+++ test/test_daxdatamodel.py
@@ -11,11 +11,42 @@ class TestTable(unittest.TestCase):
         """Add a column to a table"""
         t = Table()
         t.add_column()
+        del(t)
 
     def test_columns_should_have_valid_datatypes(self):
         """Try to get some nonsensical datatypes accepted"""
         t = Table()
-        self.assertRaises(Table.Column.InvalidDatatypeError, t.add_column, "hoi")
+        self.assertRaises(Table.Column.InvalidDatatypeError, t.add_column, "foo")
+        del(t)
+
+    def test_add_column_with_values_to_table(self):
+        """Add a column with values to the table"""
+        t = Table()
+        t.add_column_with_values("string", ["a", "b", "c"])
+        del(t)
+
+    def test_add_column_with_invalid_datatype_to_table(self):
+        """Try to add a column with not-existing datatype"""
+        t = Table()
+        self.assertRaises(Table.Column.InvalidDatatypeError, t.add_column_with_values, "foo", ["a", "b", "c"])
+        del(t)
+
+    def test_retrieve_data_from_table(self):
+        """After adding two columns with three rows, data should come out as well"""
+        t = Table()
+        t.add_column_with_values("string", ["a", "b", "c"])
+        t.add_column_with_values("integer", [1, 2, 3])
+        self.assertEqual(t.columns[0].values[1], "b")
+        self.assertEqual(t.columns[1].values[2], 3)
+        del(t)
+
+    def test_after_deleting_and_recreating_new_table_should_be_empty(self):
+        """Python should copy the lists provided (not just link them). Otherwise, newly created tables might suddenly contain data."""
+        t = Table()
+        t.add_column_with_values("string", ["a", "b", "c"])
+        del(t)
+        t = Table()
+        self.assertEqual(len(t.columns), 0)
 
 class TestDatamodel:
     def test_create_datamodel(self):
