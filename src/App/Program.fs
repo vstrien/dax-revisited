347c5a1cf5bbc5d21c095f31d813ebe9d16f3075
Koos van Strien <vstrien@Kooss-MacBook-Pro.fritz.box>
2020-11-12 21:54:00 +0100

    Inlaad-logica

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -3,10 +3,15 @@ open System.IO.Compression
 open Newtonsoft.Json
 open System.Text
 open PBIDataModelMetadata
+open FSharp.Data
 
 type ColumnData<'T> = {
     column: Column
     values: list<'T>
+}
+
+type DataTable = {
+    columns: list<ColumnData<obj>>
 }
 
 let ExtractModel sourcefile =
@@ -14,13 +19,45 @@ let ExtractModel sourcefile =
     let sr = new StreamReader(archive.GetEntry("DataModelSchema").Open (), Encoding.Unicode)
     JsonConvert.DeserializeObject<DataModelSchema>(sr.ReadToEnd())
 
+let LoadData (sourcefolder: string, table: Table) =
+    let tablename = table.name
+    let filepath = sprintf "/Users/vstrien/src/hobby/dax-revisited/sample-file/csv/%s.csv" tablename
+    let tablecontents = CsvFile.Load(filepath)
+    
+    let tableColumnDataList = [
+        for column in table.columns do
+            printfn "Trying to get column index of %s " column.name
+            if tablecontents.TryGetColumnIndex(column.name) <> None then
+                printfn "Column %s present. Loading ..." column.name
+                let columnIndex = tablecontents.GetColumnIndex(column.name)
+                let columnValues = [
+                    for row in tablecontents.Rows do
+                        row.[columnIndex]
+                ]
+                
+                printfn "%d rows loaded" columnValues.Length
+
+                let columnData = {
+                    column = column
+                    values = columnValues
+                }
+                columnData
+    ]
+
+    tableColumnDataList
 [<EntryPoint>]
 let main argv =
     printfn "Loading.."
 
-    for arg in argv do
-        let value = ExtractModel arg
-        for table in value.model.tables do
-            printfn "Table %s" (table.name)
+    let pbitfile = argv.[0]
+    let value = ExtractModel pbitfile
+    let sourcefolder = argv.[1]
 
+    let all_data = [
+        for table in value.model.tables do
+            printfn "Loading data for %s" table.name
+            LoadData (sourcefolder, table)
+    ]
+
+    
     0 // return an integer exit code
